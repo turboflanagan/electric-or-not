f062d2f32717a63313307fda3ff6d9339a7e9517
turboflanagan <turboflanagan@gmail.com>
2015-12-08 17:00:41 -0500

    add photos and vote function

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,37 @@ var MongoClient = require('mongodb').MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	MongoClient.connect('mongodb://localhost:27017/vehicle', function(error, db){
-		db.collection('vehicle').find().toArray(function(error, result){
-			console.log(result);
-		});
-	});
+	MongoClient.connect('mongodb://localhost:27017/Galant', function(error, db){
+		//1. Get all pictures from the MongoDB
+		db.collection('Galant_images').find().toArray(function(error, result){
+			allPhotos = result;
+			//2. Get the current user from MongoDB vai req.ip
+			var currIP = req.ip;
+
+			db.collection('users').find({ip:currIP}).toArray(function(error, userResult){
+				// console.log(allPhotos);
+				console.log(userResult);
+
+					for(i=0; i<userResult.length; i++){
+						
+					}
+				}
+			});
+
+
+
+			//5. Choose a random image from teh array and set it to a var
+			var rand = Math.floor(Math.random()*result.length);
+			//6. res.render the index view and send it the pic
+			res.render('index', { photo: result[rand] });
+		})
+	});	
+
+	
+	//3. Find which photos the current user has NOT voted on
+	//4. Load all those photos into an array.
+
+});;
 
 	//index page should load a random picture/item
 	//1. Get all pictures from the MongoDB
@@ -26,41 +52,44 @@ router.get('/', function(req, res, next) {
 	//4. Pass var to view
 	//5. Display 1 random image
 
-	var serverPhotos = [
-	{name: 'images/1.jpeg'},
-	{name: 'images/2.jpeg'},
-	{name: 'images/3.jpeg'},
-	{name: 'images/4.jpeg'},
-	{name: 'images/5.jpeg'},
-	{name: 'images/6.jpeg'},
-	{name: 'images/7.jpeg'},
-	{name: 'images/8.jpeg'},
-	{name: 'images/9.jpeg'},
-	{name: 'images/10.jpeg'},
-	{name: 'images/11.jpeg'},
-	{name: 'images/12.jpeg'},
-	{name: 'images/13.jpeg'},
-	{name: 'images/14.jpeg'},
-	{name: 'images/15.jpeg'},
-	{name: 'images/16.jpeg'},
-	{name: 'images/17.jpeg'},
-	{name: 'images/18.jpeg'},
-	{name: 'images/19.jpeg'},
-	{name: 'images/20.jpeg'},
-	];
 
-  res.render('index', { photos: serverPhotos });
-});
-
-router.get('/standings', function(req, res, next){
+	router.get('/standings', function(req, res, next) {
 	//1. get ALL the photos
-	//2. Sort them by hightse likes
-	//3. res.render the standings view and pass it the sorted photo array
+	//2. Sort them by highest likes
+	//3. res.render the standings view and pass it the sorted photo array 
 	res.render('index', {title: 'Standings'});
-
 });
 
-router.post('*', function(req, res, next){
-	//this will run for all posted pages
+router.get('/electric', function(req, res, next) {
+	res.render('index', {pic: 'Standings'});
 });
+
+router.post('/electric', function(req,res,next){
+	MongoClient.connect('mongodb://localhost:27017/Galant', function(error, db){
+		db.collection('users').insertOne( {
+	      ip: req.ip,
+	      vote: "electric",
+	      image: req.body.photo
+		});
+		res.redirect('../');
+	});	
+});
+
+router.post('/poser', function(req,res,next){
+	MongoClient.connect('mongodb://localhost:27017/Galant', function(error, db){
+		db.collection('users').insertOne( {
+	      ip: req.ip,
+	      vote: "poser",
+	      image: req.body.photo
+		});
+		res.redirect('../');
+	});	
+});
+
+function vote(req, res){
+}
+
 module.exports = router;
+
+
+
